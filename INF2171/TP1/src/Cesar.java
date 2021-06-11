50fedfd5d5d7b83f6d51643a1395bc3f7edd8cf8
[Ahmed Lakkati] <[ahmedlakkati@gmail.com]>
2021-06-11 01:17:25 -0400

    décalage positif seulement

--- INF2171/TP1/src/Cesar.java
+++ INF2171/TP1/src/Cesar.java
@@ -1,25 +1,37 @@
 
 import java.util.Scanner;
+
+import static java.net.IDN.toASCII;
+
 public class Cesar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Entrez la clé : ");
         int num = scan.nextInt();
+
         System.out.print("Entrez le message : ");
         String text = scan.next();
         scan.close();
-        String code ="";
-
-
-        for(int i=0; i < text.length(); i++){
-            char traitement = text.charAt(i );
-
-            code += String.valueOf(traitement + num);
-
-
+        if (num >0){
+        for(int i=0; i < text.length(); i++) {
+            char traitement = text.charAt(i);
+            if (traitement > 96 && traitement < 123) {
+                char code = 0;
+                code += ((traitement + num - 97) % 26) + 97;
+                char ch = (char) code;
+                System.out.print(code);
+            } else if (traitement > 64 && traitement < 91) {
+                char code = 0;
+                code += ((traitement + num - 65) % 26) + 65;
+                char ch = (char) code;
+                System.out.print(code);
+            }
+        }
+        }else if (num <0){
+            System.out.println("work in progress");
 
         }
-        System.out.println(code);
     }
 }
+
 
